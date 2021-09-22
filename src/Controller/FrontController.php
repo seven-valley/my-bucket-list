7b44a93c08c871f2a2bfeccbe27309685b4f7e7c
Seven Valley <augure@gmail.com>
2021-09-22 14:27:41 +0200

    ajout Wish

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -52,6 +55,28 @@ class FrontController extends AbstractController
 
         return $this->render('front/detail.html.twig', [
             'wish' => $wish,
+        ]);
+    }
+
+    /**
+     * @Route("/ajouter", name="ajouter")
+     */
+    public function ajouter(Request $request,EntityManagerInterface $em): Response
+    {
+        $wish = new Wish();
+        // relier $wish au formulaire
+        $formWish = $this->createForm(WishType::class,$wish);
+        // Hydrater le $wish
+        $formWish->handleRequest($request);
+        if ( $formWish->isSubmitted()){
+            $wish->setIsPublished(true);
+            $wish->setDateCreated(new \DateTime());
+            $em->persist($wish);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('back/ajouter.html.twig', [
+            'formWish' =>  $formWish->createView(),
         ]);
     }
 }
